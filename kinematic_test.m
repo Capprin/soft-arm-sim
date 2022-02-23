335cf1d0c239d4a1bfd9dc31fe7b79a2f576a7b8
Capprin Bass <capprin@gmail.com>
2022-02-23 11:56:28 -0800

    Added link Jacobian, did test

--- kinematic_test.m
+++ kinematic_test.m
@@ -4,8 +4,9 @@
 % initialization
 link_axes = {'z', 'z', 'z'};
 link_lengths = [0.1 0.5 0.5];
+link_coms = [0.5 0.5 0.5];
 joint_axes = {'z', 'y', 'y'};
-joint_angles = [0 pi/4 pi/2];
+joint_angles = [pi/4 pi/4 pi/2];
 
 % put together geometry
 group = SE3;
@@ -13,12 +14,19 @@ geom = arm_geometry(group, link_axes, link_lengths, joint_axes);
 
 % get Jacobian, global joint transforms
 [J, joint_trans] = arm_jacobian(group, geom, joint_angles);
+[J_com, link_trans] = link_jacobian(group, geom, joint_angles, link_coms);
 
 % get global joint locations
 joint_vecs = zeros(3,numel(joint_trans));
 for i = 1:numel(joint_trans)
     se3_vec = group.g_vec(joint_trans{i});
     joint_vecs(:,i) = se3_vec(1:3);
+end
+% get global link locations
+link_vecs = zeros(3,numel(link_trans));
+for i = 1:numel(link_trans)
+    se3_vec = group.g_vec(link_trans{i});
+    link_vecs(:,i) = se3_vec(1:3);
 end
 
 % plot
@@ -32,7 +40,7 @@ view(3);
 % arm
 plot3(joint_vecs(1,:), joint_vecs(2,:), joint_vecs(3,:), 'Color', 'k',...
       'LineStyle', '-', 'Marker', '.', 'LineWidth', 5, 'MarkerSize', 30);
-% Jacobian
+% Jacobians
 for i = 1:numel(J)
     % one arrow for ea. joint contribution
     quiver3(joint_vecs(1,i+1), joint_vecs(2,i+1), joint_vecs(3,i+1),...
@@ -41,4 +49,13 @@ for i = 1:numel(J)
             J{i}(1,2), J{i}(2,2), J{i}(3,2),'y', 'LineWidth', 2);
     quiver3(joint_vecs(1,i+1), joint_vecs(2,i+1), joint_vecs(3,i+1),...
             J{i}(1,3), J{i}(2,3), J{i}(3,3),'b', 'LineWidth', 2);
+end
+for i = 1:numel(J_com)
+    % one arrow for ea. joint contribution
+    quiver3(link_vecs(1,i), link_vecs(2,i), link_vecs(3,i),...
+            J_com{i}(1,1), J_com{i}(2,1), J_com{i}(3,1),'r', 'LineWidth', 2);
+    quiver3(link_vecs(1,i), link_vecs(2,i), link_vecs(3,i),...
+            J_com{i}(1,2), J_com{i}(2,2), J_com{i}(3,2),'y', 'LineWidth', 2);
+    quiver3(link_vecs(1,i), link_vecs(2,i), link_vecs(3,i),...
+            J_com{i}(1,3), J_com{i}(2,3), J_com{i}(3,3),'b', 'LineWidth', 2);
 end
---
+++ link_jacobian.m
@@ -0,0 +1,46 @@
+% produces Jacobian of specified points along each link
+% similar to arm_jacobian.m, with additional partial-link logic
+function [J_cell, trans] = link_jacobian(group, geometry, joint_angles, link_props)
+    % setup
+    J_cell = cell(size(link_props));
+    trans = cell(size(link_props));
+    % get joint transforms
+    joint_trans = cell(size(joint_angles));
+    for i = 1:numel(joint_trans)
+        joint_trans{i} = expm(geometry.joints{i}*joint_angles(i));
+    end
+    % get partial links
+    link_trans = cell(size(link_props));
+    for i = 1:numel(link_props)
+        mask_row = [1 1 1 link_props(i)];
+        mask = [mask_row; mask_row; mask_row; 1 1 1 1];
+        link_trans{i} = geometry.links{i} .* mask;
+    end
+    % compute J at each partial link
+    for i = 1:numel(link_props)
+        J_cell{i} = zeros(group.dim, length(joint_angles));
+        % keep track of current location
+        g = eye(size(group.g_mat([0 0 0 0 0 0])));
+        % columns of J are contributions from ea. prev. joint
+        for j = 1:i
+            % compute transform between current location and PoI
+            h = eye(size(g));
+            for k = j:i
+                % use partial link if at end
+                if j == i
+                    h = h * joint_trans{k} * link_trans{k};
+                else
+                    h = h * joint_trans{k} * geometry.links{k};
+                end
+            end
+            % get Jacobian column as a transform
+            J_mat = g * geometry.joints{j} * h;
+            % convert to vector
+            J_vec = group.g_vec(J_mat);
+            J_cell{i}(:,j) = J_vec;
+            % update g
+            trans{j} = g * joint_trans{j} * link_trans{j};
+            g = g * joint_trans{j} * geometry.links{j};
+        end
+    end
+end
